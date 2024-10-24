74db111cd647e0b9850f0b10a66a4bc205e436eb
ahmedelsersy10 <ahmedelsersy10@yahoo.com>
2024-10-24 09:41:40 +0300

    change theme and carete nots view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nots_app/views/nots_view.dart';
 
 void main() {
   runApp(const NotsApp());
@@ -9,8 +10,9 @@ class NotsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const NotsView(),
+    );
   }
 }
-
-// git commit -m "first commit"
